e4cbf5a3f784224767939579efb9e9a58dcebb39
virupakshaK <virupaksha579@gmail.com>
2021-07-07 12:46:19 +0530

    cleaned main default branch

--- target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-
-INSERT INTO test (userName, age, location) VALUES
-  ('Aliko', 23, 'Los Angels'),
-  ('Bill', 25, 'philipin'),
-  ('Folrunsho', 22, 'Canada');
-
---INSERT INTO USER(id, user_name, password, enabled, roles) values(2, 'sai', '2a$10$PIjJgMcFg33.iUhS3xWOZ.kaoyrWkFYkXpj0n1iLJV1llhmDozvwS', true, 'ROLE_USER');
---INSERT INTO USER(id, user_name, password, enabled, roles) values(1, 'virupaksha', '2a$10$PIjJgMcFg33.iUhS3xWOZ.kaoyrWkFYkXpj0n1iLJV1llhmDozvwS', true, 'ROLE_USER, ROLE_ADMIN')
